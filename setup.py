6ead43c48b27c005c9086e20a239b572df4ff430
Marcos <marcosglasner@gmail.com>
2023-04-22 12:32:48 +0100

    Updating files

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='Project Euler',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='mfg0914',
+    author_email='',
+    description=''
+)
